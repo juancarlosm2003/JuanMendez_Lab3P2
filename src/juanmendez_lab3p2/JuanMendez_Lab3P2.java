2bffddcde5b2e5ae7139aa9808026ad871f6e628
juancarlosm2003 <juancarlosmendezelias200@gmail.com>
2022-05-06 13:49:45 -0600

    Primer commit

---
+++ src/juanmendez_lab3p2/JuanMendez_Lab3P2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juanmendez_lab3p2;
+
+/**
+ *
+ * @author Juan Carlos Mendez
+ */
+public class JuanMendez_Lab3P2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
