75c28129ccfc7e9f7bc3ba2da976589a9f39a352
juancarlosm2003 <juancarlosmendezelias200@gmail.com>
2022-05-06 14:21:40 -0600

    creando el sistema

---
+++ src/juanmendez_lab3p2/Cliente.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juanmendez_lab3p2;
+
+/**
+ *
+ * @author Juan Carlos Mendez
+ */
+public class Cliente {
+    
+}
--- src/juanmendez_lab3p2/JuanMendez_Lab3P2.java
+++ src/juanmendez_lab3p2/JuanMendez_Lab3P2.java
@@ -5,6 +5,9 @@
  */
 package juanmendez_lab3p2;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Juan Carlos Mendez
@@ -15,7 +18,21 @@ public class JuanMendez_Lab3P2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         int opcion = 1;
+        ArrayList jugadores = new ArrayList();
+        while (opcion != 5) {
+            opcion = Integer.parseInt(
+                    JOptionPane.showInputDialog("Sistema de bienes raices \n"
+                            + "1.- Ingresar o eliminar clientes \n"
+                            + "2.- administrar casas\n "
+                            + "3.- Administrar departamento\n"
+                            + "4.- Administrar edificio \n"
+                            + "5.- Salir del sistema \n")
+            );
+            if (opcion == 1){
+                
+            }
+        }
     }
     
 }
